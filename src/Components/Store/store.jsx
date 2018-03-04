841dc04a46d7446957b591064c1b17729da85772
Brian Larson <manateeman24@hotmail.com>
2018-03-04 13:47:13 -0700

    something

--- src/Components/Store/store.jsx
+++ src/Components/Store/store.jsx
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
-import Header from '../Header/header'; 
-import ProductCards from '../OliveCards/oliveCards';   
+import Header from '../Header/header';
+import ProductCards from '../OliveCards/oliveCards';
 
 class Store extends Component {
-    constructor(){
-        super()
-            this.state={
-                productArr: null
-            }
-    }
+  constructor() {
+    super();
+    this.state = {
+      productArr: null
+    };
+  }
 
-    componentDidMount(){
+  componentDidMount() {}
 
-    }
-
-    
-    render() {
-        return (
-            <div>
-                <Header/>
-                <ProductCards/>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Header />
+        <ProductCards />
+      </div>
+    );
+  }
 }
 
 export default Store;
